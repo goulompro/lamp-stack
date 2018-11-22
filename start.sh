5bc44fed3169dfe2e47d7f3f14ed115d6e02a590
GOULOMpro <45230483+goulompro@users.noreply.github.com>
2018-11-22 13:01:39 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@
 service ssh start
 service apache2 start
 service mysql start
-useradd --disabled-password webadmin --ingroup root
+useradd --disabled-password webadmin --ingroup sudo
 echo '-e "root\nroot" | passwd webadmin'
